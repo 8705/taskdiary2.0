0ffbc0dd4a4bfbb50c27318e9d274e573c5701da
takobonge <shungo.ishino@gmail.com>
2015-05-10 21:46:59 +0900

    backbonejs取り込み

---
+++ app/assets/javascripts/backbonejs/collections/top.js
@@ -0,0 +1,35 @@
+// The collection of todos is backed by *localStorage* instead of a remote
+// server.
+MyApp.Collections.TodayList = Backbone.Collection.extend({
+
+  // Reference to this collection's model.
+  model: MyApp.Models.Todo,
+
+  // Save all of the todo items under the `"todos-backbone"` namespace.
+  //localStorage: new Backbone.LocalStorage("todos-backbone"),
+  url: '/tasks',
+
+  // Filter down the list of all todo items that are finished.
+  done: function() {
+    return this.where({done: true});
+  },
+
+  // Filter down the list to only todo items that are still not finished.
+  remaining: function() {
+    return this.where({done: false});
+  },
+
+  // We keep the Todays in sequential order, despite being saved by unordered
+  // GUID in the database. This generates the next order number for new items.
+  nextOrder: function() {
+    if (!this.length) return 1;
+    return this.last().get('order') + 1;
+  },
+
+  // Todays are sorted by their original insertion order.
+  comparator: 'order'
+
+});
+
+// Create our global collection of **Todays**.
+var Todays = new MyApp.Collections.TodayList();
---
+++ app/assets/javascripts/templates/top/index.js
@@ -0,0 +1,87 @@
+$(function(){
+
+  // Our overall **AppView** is the top-level piece of UI.
+  MyApp.App = Backbone.View.extend({
+
+    // Instead of generating a new element, bind to the existing skeleton of
+    // the App already present in the HTML.
+    el: $("#todoapp"),
+
+    // Delegated events for creating new items, and clearing completed ones.
+    events: {
+      "keypress #new-todo":  "createOnEnter",
+      "click #clear-completed": "clearCompleted",
+      "click #toggle-all": "toggleAllComplete"
+    },
+
+    // At initialization we bind to the relevant events on the `Todays`
+    // collection, when items are added or changed. Kick things off by
+    // loading any preexisting todos that might be saved in *localStorage*.
+    initialize: function() {
+
+      this.input = this.$("#new-todo");
+
+      this.listenTo(Todays, 'add', this.addOne);
+      this.listenTo(Todays, 'reset', this.addAll);
+      this.listenTo(Todays, 'all', this.render);
+
+      this.footer = this.$('footer');
+      this.main = $('#main');
+
+      Todays.fetch();
+    },
+
+    // Re-rendering the App just means refreshing the statistics -- the rest
+    // of the app doesn't change.
+    render: function() {
+      var done = Todays.done().length;
+      var remaining = Todays.remaining().length;
+
+      if (Todays.length) {
+        this.main.show();
+        this.footer.show();
+      } else {
+        this.main.hide();
+        this.footer.hide();
+      }
+    },
+
+    // Add a single todo item to the list by creating a view for it, and
+    // appending its element to the `<ul>`.
+    addOne: function(todo) {
+      var view = new MyApp.Views.Task({model: todo});
+      this.$("#todayTasks").append(view.render().el);
+    },
+
+    // Add all items in the **Todays** collection at once.
+    addAll: function() {
+      Todays.each(this.addOne, this);
+    },
+
+    // If you hit return in the main input field, create new **Todo** model,
+    // persisting it to *localStorage*.
+    createOnEnter: function(e) {
+      if (e.keyCode != 13) return;
+      if (!this.input.val()) return;
+
+      Todays.create({title: this.input.val()});
+      this.input.val('');
+    },
+
+    // Clear all done todo items, destroying their models.
+    clearCompleted: function() {
+      _.invoke(Todays.done(), 'destroy');
+      return false;
+    },
+
+    toggleAllComplete: function () {
+      var done = this.allCheckbox.checked;
+      Todays.each(function (todo) { todo.save({'done': done}); });
+    }
+
+  });
+
+  // Finally, we kick things off by creating the **App**.
+  var App = new MyApp.App();
+
+});
